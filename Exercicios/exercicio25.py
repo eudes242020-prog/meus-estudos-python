4875f2183e3aa6eea0e50013e250130059e664c1
Eudes <eudes242020@gmail.com>
2025-12-16 14:43:06 -0300

    Adicionando notas de estudo sobre geradores

--- Exercicios/exercicio25.py
+++ Exercicios/exercicio25.py
@@ -1,14 +1,13 @@
-# Código do Estagiário (Perigo!)
-# Ele cria uma lista GIGANTE na memória só para pegar o primeiro item depois.
-
-import sys # Biblioteca para ver o tamanho das coisas
-
-# 1. LISTA (Usa colchetes [])
-# O Python cria TODOS os números e salva na memória agora.
-lista = [n for n in range(1000000)]
-print(f"Tamanho da Lista na RAM: {sys.getsizeof(lista)} bytes")
+# TESTE DE MEMÓRIA: LISTA vs GERADOR
+# Aprendizado do dia: Listas carregam tudo na RAM de uma vez.
+# Geradores (usando parênteses) criam sob demanda (Lazy Evaluation).
+#
+# Ruim para memória pois útiliza uma quantidade muito alta (List Comprehension [])
+# ainda não me aprofudei nesse assunto mais em breve voltaria para dizer que entendi tudo em 100%
+# lista_pesada = [x for x in range(10000000)]
 
 # 2. GERADOR (Usa parênteses ())
 # O Python NÃO cria os números. Ele só guarda a "receita".
+import sys
 gerador = (n for n in range(1000000))
 print(f"Tamanho do Gerador na RAM: {sys.getsizeof(gerador)} bytes")
