35255d72aec654d3ecf53564c2f293fed6c0a130
Junio <eudes242020@gmail.com>
2026-07-14 21:00:21 -0300

    retirado o flask-cors do requirements.txt e do app.py

--- projetos/tarefas/app.py
+++ projetos/tarefas/app.py
@@ -1,10 +1,8 @@
 import sqlite3
-from flask_cors import CORS
 from flask import Flask, jsonify, request, render_template
 from dados_tarefas import carregar_tarefas, salvar_tarefas, conexao_api, concluir_tarefa,remover_tarefa
 from tarefas import Tarefa
 app=Flask(__name__)
-CORS(app)
 @app.route("/tarefas")
 def listar_tarefas():
     return jsonify(transporte_api(carregar_tarefas()))
